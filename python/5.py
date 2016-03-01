cb3999c0d20a01e42c00e5fd79b0860efc82b93f
Amol Dharmadhikari <amol.dharmadhikari@gmail.com>
2016-03-01 12:12:07 -0800

    incremental 3 - project euler - python

--- python/5.py
+++ python/5.py
@@ -7,11 +7,57 @@
 # https://projecteuler.net/problem=5
 
 import string, sys
+import functools
 from math import sqrt
+
+def isPrime(n):
+    """
+    Checks if the number is prime
+    """
+    # Return false if numbers are less than 2
+    if n < 2:
+        return False
+
+    # 2 is smallest prime
+    if n == 2:
+        return True
+
+    # All even numbers are not prime
+    if not n & 1:
+        return False
+
+    # Now start at 3, go upto the square root of the number and check
+    # for divisibility. Do this in steps of two so that we consider
+    # only odd numbers
+    for i in range(3, int(n**0.5)+1, 2):
+        if n%i == 0:
+            return False
+
+    # number is prime
+    return True
+
+def getPrimeFactors(n):
+    """
+    Return a list having the prime factors of the number including the 
+    number itself
+    """
+    factors = []
+
+    for i in range(n+1):
+        if isPrime(i) and n%i == 0:
+            factors.append(i)
+
+    return factors
 
 def main():
     # we need to find least common multiple of all numbers from 1-20
-    pass
+    factors = {}
+
+    # Get the factors of all the numbers. We can skip 1
+    for i in range(2, 21):
+        factors[i] = getPrimeFactors(i)
+
+    print(factors)
 
 if __name__ == '__main__':
     main()
