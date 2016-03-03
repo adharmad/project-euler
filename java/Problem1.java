94d8ff549a14e97723260fdc12db61484844b385
Amol Dharmadhikari <amol.dharmadhikari@gmail.com>
2016-03-02 16:10:58 -0800

    problem #6,7,8

--- java/Problem1.java
+++ java/Problem1.java
@@ -1,4 +1,3 @@
-
 public class Problem1 {
 	public static void main(String[] args) {
 		int sum = 0;
--- python/6.py
+++ python/6.py
@@ -1,9 +1,9 @@
 # The sum of the squares of the first ten natural numbers is,
 # 
-# 12 + 22 + ... + 102 = 385
+# 1^2 + 2^2 + ... + 10^2 = 385
 # The square of the sum of the first ten natural numbers is,
 #
-# (1 + 2 + ... + 10)2 = 552 = 3025
+# (1 + 2 + ... + 10)^2 = 552 = 3025
 # Hence the difference between the sum of the squares of the first ten 
 # natural numbers and the square of the sum is 3025 − 385 = 2640.
 
@@ -17,7 +17,18 @@ import functools
 from math import sqrt
 
 def main():
-    pass
+    sum_of_squares = 0
+    square_of_sum = 0
+    sum = 0
+
+    for i in range(101):
+        sum_of_squares += i*i
+        sum += i
+
+    square_of_sum = sum*sum
+    diff = square_of_sum - sum_of_squares
+
+    print(diff)
 
 if __name__ == '__main__':
     main()
