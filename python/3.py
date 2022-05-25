040342bc3fbc9501138a3bd952274c7f617d0225
Amol Dharmadhikari <adharmad@ionate.io>
2022-05-24 17:29:00 -0700

    Minor bug in 3.py

--- python/3.py
+++ python/3.py
@@ -1,10 +1,10 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 #
-# What is the largest prime factor of the number 600851475143 ? 
+# What is the largest prime factor of the number 600851475143 ?
 #
 # https://projecteuler.net/problem=3
 
-import string, sys
+import string, sys, math
 import functools
 
 import commonutils
@@ -13,7 +13,7 @@ def main():
     last = 0
     lst = []
     num = 600851475143
-    for i in range(2, int(sqrt(num))):
+    for i in range(2, int(math.sqrt(num))):
         if not commonutils.isPrime(i):
             continue
         elif commonutils.isPrime(i) and num%i == 0:
