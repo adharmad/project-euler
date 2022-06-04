68df74ab33abd10c8e833a1830a63a19d13d3705
Amol Dharmadhikari <adharmad@ionate.io>
2022-06-03 17:29:00 -0700

    7.py formatted print

--- python/7.py
+++ python/7.py
@@ -1,4 +1,4 @@
-# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can 
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
 # see that the 6th prime is 13.
 #
 # What is the 10001st prime number?
@@ -15,13 +15,13 @@ def main():
     while True:
         if commonutils.isPrime(num):
             count += 1
-       
+
         if count == 10001:
             break
 
         num += 1
 
-    print("The 10001th prime number = ", num)
+    print(f"The 10001th prime number = {num}")
 
 if __name__ == '__main__':
     main()
