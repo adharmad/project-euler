5cf9b2382cbaa9f30db190e0f7d6fd3d1d414fc5
Amol Dharmadhikari <adharmad@ionate.io>
2022-09-04 11:29:14 -0700

    print formatting, etc.

--- python/14.py
+++ python/14.py
@@ -1,19 +1,19 @@
-# The following iterative sequence is defined for the set of positive 
+# The following iterative sequence is defined for the set of positive
 # integers:
-# 
+#
 # n → n/2 (n is even)
 # n → 3n + 1 (n is odd)
-# 
-# Using the rule above and starting with 13, we generate the following 
+#
+# Using the rule above and starting with 13, we generate the following
 # sequence:
 # 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-# 
-# It can be seen that this sequence (starting at 13 and finishing at 1) 
-# contains 10 terms. Although it has not been proved yet (Collatz 
+#
+# It can be seen that this sequence (starting at 13 and finishing at 1)
+# contains 10 terms. Although it has not been proved yet (Collatz
 # Problem), it is thought that all starting numbers finish at 1.
-# 
+#
 # Which starting number, under one million, produces the longest chain?
-# 
+#
 # NOTE: Once the chain starts the terms are allowed to go above one million.
 #
 # https://projecteuler.net/problem=14
@@ -43,10 +43,12 @@ def main():
         if len(chain) > len(longestChain):
             longestChain = chain
             longestChainFor = i
-            print ("New longest chain for number", i, " is ", getChain(i))
+            longestChainLength = len(chain)
+            #print (f"New longest chain for number {i} is {chain}")
+            print (f"New longest chain length for number {i} is {longestChainLength}")
 
-    print ("Longest chain number = ", longestChainFor)
-    print ("Longest chain is ", longestChain)
+    print (f"Longest chain number = {longestChainFor}")
+    print (f"Longest chain is {longestChain}")
 
 if __name__ == '__main__':
     main()
